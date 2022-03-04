3b7157ad59a333e08e2504f01b5aa94f02f79b26
Anderson Boa Morte <anderson.boamorte@gmail.com>
2022-03-03 23:43:42 -0300

    alter pass localhost postgres

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:960113@localhost/mestrado-ifpb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:privacychain@localhost/mestrado-ifpb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'options': '-csearch_path={}'.format('privacychain')}
